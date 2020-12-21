037a0b49c4023163017800158271f2436b17c1af
Jorge Ramón <jorge.ramon2128@gmail.com>
2020-12-20 19:13:39 -0600

    Added wonky and navidad commands

---
+++ src/miscellaneous/gateway/timer.gateway.ts
@@ -0,0 +1,63 @@
+import { Command } from '@discord/decorator/command.decorator';
+import { Emotes } from '@shared/enum/emotes.enum';
+import { Injectable } from '@nestjs/common';
+import { Message } from 'discord.js';
+import { SpecialDateService } from '@shared/service/special-date.service';
+import { sample } from 'lodash';
+
+@Injectable()
+export class TimerGateway {
+  constructor(private readonly specialDateService: SpecialDateService) {}
+
+  @Command({ name: 'navidad' })
+  async daysUntilChristmas(message: Message): Promise<void> {
+    if (this.specialDateService.isChristmas()) {
+      await message.channel.send(`HOY ES NAVIDAD ${Emotes.PADORUUCHH} 🎄 🎁`, {
+        files: [
+          'https://media1.giphy.com/media/l0tzgJRjGc7mGgsmXY/giphy.gif?cid=ecf05e478f1109863cc8c875b63a3020145a8476663fb7ac&rid=giphy.gif',
+        ],
+      });
+    } else {
+      await message.channel.send(
+        this.createChristmasResponse(
+          this.specialDateService.daysUntilChristmas(),
+        ),
+      );
+    }
+  }
+
+  @Command({ name: 'wonky' })
+  async daysUntilWonkyDay(message: Message): Promise<void> {
+    if (!this.specialDateService.isWonkyDay()) {
+      const gifs = [
+        'https://media1.giphy.com/media/Pt5PnJVtHuy2s/giphy.gif?cid=ecf05e47g1u8p8ffxp01c2a16uo2xfs3nk8r8bgvkhlc9ntc&rid=giphy.gif',
+        'https://media1.giphy.com/media/l0Exhlza2TqHJzk5y/giphy.gif?cid=ecf05e47g1u8p8ffxp01c2a16uo2xfs3nk8r8bgvkhlc9ntc&rid=giphy.gif',
+        'https://media0.giphy.com/media/RHP0azDLoGmMYZHNLI/giphy.gif?cid=ecf05e47o4rb7jqzm6idyqk333ziz89rm5m12i2qm2ivfs5y&rid=giphy.gif',
+        'https://media2.giphy.com/media/804TNmnYLfNao/giphy.gif?cid=ecf05e4770ius12g8qi5jh3rvrfeq3f6lyfwtb36dpquvsf9&rid=giphy.gif',
+      ];
+
+      await message.channel.send(
+        `Hoy es el día de funar a Wonky ${Emotes.KEK}`,
+        {
+          files: [sample(gifs)],
+        },
+      );
+    } else {
+      await message.channel.send(
+        this.createWonkyResponse(this.specialDateService.daysUntilWonkyDay()),
+      );
+    }
+  }
+
+  private createWonkyResponse(days: number): string {
+    return `${days > 1 ? 'Faltan' : 'Falta'} **${days}** ${
+      days > 1 ? 'días' : 'día'
+    } para el "Martes de funar a Wonky" ${Emotes.JARMONIS_APPROVES}`;
+  }
+
+  private createChristmasResponse(days: number): string {
+    return `${days > 1 ? 'Faltan' : 'Falta'} **${days}** ${
+      days > 1 ? 'días' : 'día'
+    } para navidad ${Emotes.PADORUUCHH} 🎄 🎁`;
+  }
+}
--- src/miscellaneous/miscellaneous.module.ts
+++ src/miscellaneous/miscellaneous.module.ts
@@ -1,7 +1,10 @@
 import { ChhBotGateway } from './gateway/chhbot.gateway';
 import { Module } from '@nestjs/common';
+import { SharedModule } from '@shared/shared.module';
+import { TimerGateway } from './gateway/timer.gateway';
 
 @Module({
-  providers: [ChhBotGateway],
+  providers: [ChhBotGateway, TimerGateway],
+  imports: [SharedModule],
 })
 export class MiscellaneousModule {}

aca8dd8224cb7ecbe2801c3262dfda49e44cd5e1
Jorge Ramón <jorge.ramon2128@gmail.com>
2021-02-17 16:57:59 -0600

    Added command to show pack links

---
+++ src/shared/enum/packs.enum.ts
@@ -0,0 +1,25 @@
+export enum Packs {
+  Puppetz = 'puppetz',
+}
+
+export const PackLinks: Record<Packs, Object> = {
+  [Packs.Puppetz]: {
+    'Puppetz Hero Zero': 'https://gofile.io/d/1wNLJr',
+    'Puppetz Hero': 'https://gofile.io/d/VY75ip',
+    'Puppetz Hero 2': 'https://gofile.io/d/1wNLJr',
+    'Puppetz Hero 3': 'https://gofile.io/d/iGjrNJ',
+    'Puppetz Hero 4': 'https://gofile.io/d/QhMqEL',
+    'Puppetz Hero 5': 'https://gofile.io/d/XmKw8E',
+    'Puppetz Hero Dragonforce': 'https://gofile.io/d/s2BCH0',
+    'Puppetz Hero Megadeth': 'https://gofile.io/d/jeG2CS',
+    'Puppetz Hero Iron Maiden': 'https://gofile.io/d/SUa2PS',
+    'Puppetz Hero T': 'https://gofile.io/d/f59YWc',
+    'Cowbows From Hell (Full Album)': 'https://gofile.io/d/qmMf3O',
+    'Rock Band X': 'https://gofile.io/d/FGOyJc',
+  },
+};
+
+export const PackDescriptions: Record<Packs, string> = {
+  [Packs.Puppetz]:
+    'Puppetz es uno de los charters legendarios en la era Frets On Fire, sus packs son difíciles de conseguir. Podrás encontrar los packs de Puppetz Hero Zero, 1, 2, 3 y 4 (algunos incompletos) además de packs dedicados a Megadeth, Iron Maiden, Metallica entre otros.',
+};

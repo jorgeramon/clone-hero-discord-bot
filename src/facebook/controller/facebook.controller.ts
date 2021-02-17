aa5418cb83f45faaf15e89084faa5257716493cd
Jorge Ramón <jorge.ramon2128@gmail.com>
2021-02-17 01:50:54 -0600

    Added FB hook

--- src/facebook/controller/facebook.controller.ts
+++ src/facebook/controller/facebook.controller.ts
@@ -1,36 +1,11 @@
-import { Controller, Delete, Get, Post, Put, Req } from '@nestjs/common';
+import { Controller, Post, Req } from '@nestjs/common';
 
 @Controller('facebook')
 export class FacebookController {
-  @Get()
-  testA(@Req() request) {
-    console.log('-----------------[ Facebook GET ]-----------------------');
-    console.log(request.body);
-    console.log(request.query);
-    return 'Ok from GET';
-  }
-
   @Post()
   testB(@Req() request) {
     console.log('-----------------[ Facebook POST ]-----------------------');
-    console.log(request.body);
-    console.log(request.query);
-    return 'Ok from GET';
-  }
-
-  @Put()
-  testC(@Req() request) {
-    console.log('-----------------[ Facebook PUT ]-----------------------');
-    console.log(request.body);
-    console.log(request.query);
-    return 'Ok from GET';
-  }
-
-  @Delete()
-  testD(@Req() request) {
-    console.log('-----------------[ Facebook DEL ]-----------------------');
-    console.log(request.body);
-    console.log(request.query);
+    console.log(JSON.stringify(request.body, null, 2));
     return 'Ok from GET';
   }
 }

116242639a500d7913c2f9d57915e79a77d9bb27
Jorge Ramón <jorge.ramon2128@gmail.com>
2021-07-04 14:05:08 -0500

    Added rcon command to minecraft server

--- src/minecraft/gateway/minecraft.gateway.ts
+++ src/minecraft/gateway/minecraft.gateway.ts
@@ -1,13 +1,19 @@
 import { Command } from '@discord/decorator/command.decorator';
+import { Guards } from '@discord/decorator/guard.decorator';
 import { Injectable } from '@nestjs/common';
 import { ConfigService } from '@nestjs/config';
 import { Servers } from '@shared/enum/servers.enum';
+import { IsAdminGuard } from '@shared/guard/is-admin.guard';
 import { Message } from 'discord.js';
-import { queryFull } from 'minecraft-server-util';
+import { queryFull, RCON } from 'minecraft-server-util';
 
 @Injectable()
 export class MinecraftGateway {
-  constructor(private readonly configService: ConfigService) {}
+  private MINECRAFT_HOST: string;
+
+  constructor(private readonly configService: ConfigService) {
+    this.MINECRAFT_HOST = this.configService.get<string>('MINECRAFT_HOST');
+  }
 
   @Command({
     name: 'minecraft',
@@ -16,13 +22,12 @@ export class MinecraftGateway {
   })
   async minecraft(message: Message): Promise<void> {
     try {
-      const MINECRAFT_HOST = this.configService.get<string>('MINECRAFT_HOST');
-      const response = await queryFull(MINECRAFT_HOST);
+      const response = await queryFull(this.MINECRAFT_HOST);
 
       await message.channel.send([
         `Estatus del servidor: **En línea** ✅`,
         '',
-        `Dirección IP/Dominio: **${MINECRAFT_HOST}**`,
+        `Dirección IP/Dominio: **${this.MINECRAFT_HOST}**`,
         '',
         `Versión: **${response.version}**`,
         '',
@@ -41,8 +46,7 @@ export class MinecraftGateway {
   })
   async status(message: Message): Promise<void> {
     try {
-      const MINECRAFT_HOST = this.configService.get<string>('MINECRAFT_HOST');
-      await queryFull(MINECRAFT_HOST);
+      await queryFull(this.MINECRAFT_HOST);
 
       await message.channel.send('El servidor se encuentra en línea ✅');
     } catch (e) {
@@ -58,8 +62,7 @@ export class MinecraftGateway {
   })
   async players(message: Message): Promise<void> {
     try {
-      const MINECRAFT_HOST = this.configService.get<string>('MINECRAFT_HOST');
-      const response = await queryFull(MINECRAFT_HOST);
+      const response = await queryFull(this.MINECRAFT_HOST);
 
       await message.channel.send([
         `Hay **${response.onlinePlayers}** ${
@@ -71,6 +74,41 @@ export class MinecraftGateway {
         ...response.players,
       ]);
     } catch (e) {
+      await message.channel.send('El servidor no se encuentra disponible ❌');
+    }
+  }
+
+  @Guards(IsAdminGuard)
+  @Command({
+    name: 'rcon',
+    description:
+      'Ejecuta un comando en el servidor de minecraft a través de RCON',
+    onlyFor: Servers.PHC,
+  })
+  async command(message: Message, args: string[]): Promise<void> {
+    try {
+      const MINECRAFT_RCON_PASSWORD = this.configService.get<string>(
+        'MINECRAFT_RCON_PASSWORD',
+      );
+
+      const MINECRAFT_RCON_PORT = this.configService.get<number>(
+        'MINECRAFT_RCON_PORT',
+      );
+
+      const client = new RCON(this.MINECRAFT_HOST, {
+        password: MINECRAFT_RCON_PASSWORD,
+        port: Number(MINECRAFT_RCON_PORT),
+      });
+
+      await client.connect();
+
+      client.once('output', async () => {
+        client.close();
+      });
+
+      await client.run(args.join(' '));
+    } catch (e) {
+      console.error(e);
       await message.channel.send('El servidor no se encuentra disponible ❌');
     }
   }

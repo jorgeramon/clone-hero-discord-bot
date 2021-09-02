d4f02a7e220d0f9a5ecd9c19475cf469a728c0f3
Jorge Ramón <jorge.ramon2128@gmail.com>
2021-09-02 13:27:57 -0500

    Added command to show phc mail credentials

--- src/miscellaneous/gateway/admin.gateway.ts
+++ src/miscellaneous/gateway/admin.gateway.ts
@@ -1,18 +1,37 @@
-import * as moment from 'moment';
-
-import { Client, Message, User } from 'discord.js';
-
 import { Command } from '@discord/decorator/command.decorator';
 import { Guards } from '@discord/decorator/guard.decorator';
 import { InjectClient } from '@discord/decorator/inject-client.decorator';
 import { Injectable } from '@nestjs/common';
-import { IsAdminGuard } from '@shared/guard/is-admin.guard';
+import { ConfigService } from '@nestjs/config';
 import { Servers } from '@shared/enum/servers.enum';
+import { IsAdminGuard } from '@shared/guard/is-admin.guard';
+import { Client, Message, User } from 'discord.js';
+import * as moment from 'moment';
 
 @Injectable()
 export class AdminGateway {
   @InjectClient()
   client: Client;
+
+  constructor(private readonly configService: ConfigService) {}
+
+  @Guards(IsAdminGuard)
+  @Command({
+    name: 'gmail',
+    isAdmin: true,
+    onlyFor: Servers.PHC,
+    description:
+      'Muestra los datos de la cuenta Gmail de Plastic Hero Community',
+  })
+  async gmail(message: Message): Promise<void> {
+    const email = this.configService.get('PHC_MAIL');
+    const password = this.configService.get('PHC_PASSWORD');
+
+    await message.channel.send([
+      `Correo: **${email}**`,
+      `Contraseña: **${password}**`,
+    ]);
+  }
 
   @Guards(IsAdminGuard)
   @Command({
